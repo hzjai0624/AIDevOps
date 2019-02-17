da517c5dbdbda325a1cadf5d9e1774b3314302d5
Keita Onabuta <konabuta@Keitas-MacBook-Pro.local>
2019-02-17 23:06:11 +0900

    pep8

--- code/notebook/training/train_diabetes.py
+++ code/notebook/training/train_diabetes.py
@@ -16,26 +16,26 @@ from typing import Tuple, List
 run = Run.get_context()
 
 # データの準備 (クラウドのストレージのマウント、データダウンロードも可能)
-X, y = load_diabetes(return_X_y = True)
+X, y = load_diabetes(return_X_y=True)
 columns = ['age', 'gender', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 data = {
-    "train":{"X": X_train, "y": y_train},        
-    "test":{"X": X_test, "y": y_test}
+    "train": {"X": X_train, "y": y_train},
+    "test": {"X": X_test, "y": y_test}
 }
 
 print('モデル学習開始....')
 
 # ハイパーパラメータの設定
 alphas = np.arange(0.0, 1.0, 0.05)
-alpha=alphas[np.random.choice(alphas.shape[0], 1, replace=False)][0]
-print("alpha:",alpha)
+alpha = alphas[np.random.choice(alphas.shape[0], 1, replace=False)][0]
+print("alpha:", alpha)
 
-reg = Ridge(alpha = alpha)
+reg = Ridge(alpha=alpha)
 reg.fit(data['train']['X'], data['train']['y'])
 preds = reg.predict(data['test']['X'])
 mse = mean_squared_error(preds, data['test']['y'])
-print("mse:",mse)
+print("mse:", mse)
 
 # メトリック記録
 run.log('mse', mse)
